94e08f0224dc3d9f0f8d8d1b191456d6774bcd1e
Ricardo Erazo Muñoz <ricardo.erazo@correounivalle.edu.co>
2025-05-21 16:26:46 -0500

    minimax

--- src/components/Knight.jsx
+++ src/components/Knight.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import '../styles/Knight.css';
-import { PLAYER_HEX_COLORS, PLAYER_COLORS } from '../constants';
+import { PLAYER_COLORS } from '../constants';
 
 function Knight({ color, isSelected }) {
   let knightClass = "knight";
   if (isSelected) knightClass += " selected";
 
-  const style = {
-    color: color === PLAYER_COLORS.GREEN ? PLAYER_HEX_COLORS.GREEN : PLAYER_HEX_COLORS.RED,
-  };
+  // Usando rutas relativas desde public/images
+  const yoshiImage = color === PLAYER_COLORS.GREEN 
+    ? "/images/green-yoshi.png" 
+    : "/images/red-yoshi.png";
 
   return (
-    <div className={knightClass} style={style}>
-      ♘
+    <div className={knightClass}>
+      <img 
+        src={yoshiImage} 
+        alt={`${color === PLAYER_COLORS.GREEN ? 'Green' : 'Red'} Yoshi`} 
+        className="yoshi-image"
+      />
     </div>
   );
 }
